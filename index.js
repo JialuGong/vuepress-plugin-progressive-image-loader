21684d5f1ce3d7cf293b1d4419ff3217b3277955
Gong Jialu <huaiyugong123@gmail.com>
2021-05-24 15:23:16 +0800

    fix: fix mixin bugs

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const {resolve} =require('path')
+const { resolve, relative } = require("path");
+const { config } = require("process");
 
-
-module.exports=(_options,ctx)=>{
-    return {
-        extendMarkdown:md=>{
-            md.set({breaks:true})
-            md.use(require(resolve(__dirname, './lib/markdown')), { 'sourceDir': ctx.sourceDir })
+module.exports = (_options, ctx) => {
+  return {
+    extendMarkdown: (md) => {
+      md.set({ breaks: true });
+      md.use(require(resolve(__dirname, "./lib/markdown")), {
+        sourceDir: ctx.sourceDir,
+      });
+    },
+    chainWebpack(config, _isServer) {
+      config.module
+      .rule('vue')
+      .use("vue-loader")
+      
+      .options({
+        transformAssetUrls: {
+          video: ["src", "poster"],
+          source: "src",
+          img: ["src","data-src"],
+          image: ["xlink:href", "href"],
+          use: ["xlink:href", "href"],
         },
-        chainWebpack(config,isServer){
-            config.delete('url-loader')
-        },
-        clientRootMixin: resolve(__dirname, './lib/mixins/mixin.js')
-    }
-}
+      });
+    },
+    clientRootMixin: resolve(__dirname, "./lib/mixins/mixin.js"),
+  };
+};
--- tests/markdown-render-test.js
+++ tests/markdown-render-test.js
@@ -1,5 +1,8 @@
-const md=require('markdown-it')();
-const options = { sourceDir:'/Users/huaiyu/personal-project/host-of-loader/docs'}
-let ans=md.use(require('../markdown-it-base64'),options)
-.render('![dd](1.jpeg)')
+const md = require("markdown-it")();
+const options = {
+  sourceDir: "/Users/huaiyu/personal-project/host-of-loader/docs",
+};
+let ans = md
+  .use(require("../markdown-it-base64"), options)
+  .render("![dd](1.jpeg)");
 console.log(ans);
